758283fd112370ae139885bc398ded7849896431
Joel Singh <joelsingh788@gmail.com>
2024-06-13 20:57:22 -0400

    Estimate rewards

--- main.py
+++ main.py
@@ -21,6 +21,10 @@ levers = [
     1.0,
 ]
 
+rewardEstimates: list[None | float] = [
+    None, None, None, None, None, None, None, None, None, None
+]
+
 def randomLever():
     lever = math.floor(random.random() * 10)
     leverReward = levers[lever]
@@ -30,11 +34,19 @@ def randomLever():
     }
 
 for i in range(99999):
+    lever = randomLever()
+    leverIndex = lever["leverIndex"]
+    leverReward = lever["leverReward"]
+    if (rewardEstimates[leverIndex] is None):
+        rewardEstimates[leverIndex] = leverReward
+    else:
+        calculateNewAverageIncrementally(rewardEstimates[leverIndex], leverReward, i + 1)
+
     if (i == 0):
-        averageRewards.append(randomLever()["leverReward"])
+        averageRewards.append(leverReward)
     else:
         averageRewards.append(
-            calculateNewAverageIncrementally(averageRewards[i - 1], randomLever()["leverReward"], i + 1)
+            calculateNewAverageIncrementally(averageRewards[i - 1], leverReward, i + 1)
         )
     for lever,reward in enumerate(levers):
         levers[lever] += np.random.normal(0, 0.01)
@@ -42,4 +54,5 @@ for i in range(99999):
 plt.plot(averageRewards)
 plt.ylabel("Average rewards")
 plt.xlabel("Step")
+print(rewardEstimates)
 plt.show()
