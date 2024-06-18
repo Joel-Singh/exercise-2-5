9b1f6a510debf990e84f76ba5060af23431b3a30
Joel Singh <joelsingh788@gmail.com>
2024-06-18 18:11:57 -0400

    Plot averageRewards for incrementalAndWeighted

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ from typing import Final
 from DoRun import run
 import matplotlib.pyplot as plt
 
-NUMBER_OF_RUNS: Final = 2000
-allAverageRewards: list[list[float]] = []
-allPercentageOfOptimalLeverChosen: list[list[float]] = []
-
+NUMBER_OF_RUNS: Final = 20
+allAverageRewardsIncremental: list[list[float]] = []
+allAverageRewardsWeighted: list[list[float]] = []
 
 def getSingleListOfAverages(listContainingListsOfAverages: list[list[float]]):
     singleListOfAverages: list[float] = []
@@ -19,11 +18,16 @@ def getSingleListOfAverages(listContainingListsOfAverages: list[list[float]]):
 
 for i in range(NUMBER_OF_RUNS):
     singleRun = run(useIncrementalEstimateCalculation=True)
-    allAverageRewards.append(singleRun['averageRewards'])
-    allPercentageOfOptimalLeverChosen.append(singleRun['percentageOfOptimalLeverChosen'])
+    allAverageRewardsIncremental.append(singleRun['averageRewards'])
     print(str(round(((i + 1) / NUMBER_OF_RUNS) * 100, 2)) + "%")
 
-plt.plot(getSingleListOfAverages(allAverageRewards))
+for i in range(NUMBER_OF_RUNS):
+    singleRun = run(useIncrementalEstimateCalculation=False)
+    allAverageRewardsWeighted.append(singleRun['averageRewards'])
+    print(str(round(((i + 1) / NUMBER_OF_RUNS) * 100, 2)) + "%")
+
+plt.plot(getSingleListOfAverages(allAverageRewardsIncremental), 'r')
+plt.plot(getSingleListOfAverages(allAverageRewardsWeighted), 'b')
 plt.ylabel("Average reward over " + str(NUMBER_OF_RUNS) + " runs")
 plt.xlabel("Step")
 plt.show()
