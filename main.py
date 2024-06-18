0af1f0f0cde1ec65df36ef22cb67a545ecaafe97
Joel Singh <joelsingh788@gmail.com>
2024-06-17 22:20:20 -0400

    Do 2000 runs and put them in an array

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+from typing import Final
 from DoRun import run
 
-singleRun = run(False)
-print(singleRun['averageRewards'])
-print(singleRun['percentageOfOptimalLeverChosen'])
+NUMBER_OF_RUNS: Final = 2000
+allAverageRewards: list[list[float]] = []
+allPercentageOfOptimalLeverChosen: list[list[float]] = []
+
+for i in range(NUMBER_OF_RUNS):
+    singleRun = run(True)
+    allAverageRewards.append(singleRun['averageRewards'])
+    allPercentageOfOptimalLeverChosen.append(singleRun['percentageOfOptimalLeverChosen'])
+    print(str(round(((i + 1) / NUMBER_OF_RUNS) * 100, 2)) + "%")
