4f61d585671d1c053dcc0372f7203fdbec3a7927
Joel Singh <joelsingh788@gmail.com>
2024-06-18 18:29:47 -0400

    Change NUMBER_OF_RUNS 100 -> 2000

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from typing import Final
 from DoRun import run
 import matplotlib.pyplot as plt
 
-NUMBER_OF_RUNS: Final = 100
+NUMBER_OF_RUNS: Final = 2000
 allAverageRewardsIncremental: list[list[float]] = []
 allAverageRewardsWeighted: list[list[float]] = []
 
