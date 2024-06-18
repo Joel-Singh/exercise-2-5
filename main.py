c517c8aeaef93dd96cd90352b34c2c9d40c6e179
Joel Singh <joelsingh788@gmail.com>
2024-06-18 18:28:53 -0400

    Plot percentageOfOptimalLeverChosen too

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ from typing import Final
 from DoRun import run
 import matplotlib.pyplot as plt
 
-NUMBER_OF_RUNS: Final = 20
+NUMBER_OF_RUNS: Final = 100
 allAverageRewardsIncremental: list[list[float]] = []
 allAverageRewardsWeighted: list[list[float]] = []
+
+allPercentageOfOptimalLeverChosenIncremental: list[list[float]] = []
+allPercentageOfOptimalLeverChosenWeighted: list[list[float]] = []
 
 def getSingleListOfAverages(listContainingListsOfAverages: list[list[float]]):
     singleListOfAverages: list[float] = []
@@ -19,15 +22,25 @@ def getSingleListOfAverages(listContainingListsOfAverages: list[list[float]]):
 for i in range(NUMBER_OF_RUNS):
     singleRun = run(useIncrementalEstimateCalculation=True)
     allAverageRewardsIncremental.append(singleRun['averageRewards'])
-    print(str(round(((i + 1) / NUMBER_OF_RUNS) * 100, 2)) + "%")
+    allPercentageOfOptimalLeverChosenIncremental.append(singleRun['percentageOfOptimalLeverChosen'])
+    print(str(round(((i + 1) / (NUMBER_OF_RUNS * 2)) * 100, 2)) + "%")
 
 for i in range(NUMBER_OF_RUNS):
     singleRun = run(useIncrementalEstimateCalculation=False)
     allAverageRewardsWeighted.append(singleRun['averageRewards'])
-    print(str(round(((i + 1) / NUMBER_OF_RUNS) * 100, 2)) + "%")
+    allPercentageOfOptimalLeverChosenWeighted.append(singleRun['percentageOfOptimalLeverChosen'])
+    print(str(round(((i + 1 + NUMBER_OF_RUNS) / (NUMBER_OF_RUNS * 2)) * 100, 2)) + "%")
 
+plt.subplot(121)
 plt.plot(getSingleListOfAverages(allAverageRewardsIncremental), 'r')
 plt.plot(getSingleListOfAverages(allAverageRewardsWeighted), 'b')
 plt.ylabel("Average reward over " + str(NUMBER_OF_RUNS) + " runs")
 plt.xlabel("Step")
+
+plt.subplot(122)
+plt.plot(getSingleListOfAverages(allPercentageOfOptimalLeverChosenIncremental), 'r')
+plt.plot(getSingleListOfAverages(allPercentageOfOptimalLeverChosenWeighted), 'b')
+plt.ylabel("Average percentage of optimal lever chosen over " + str(NUMBER_OF_RUNS) + " runs")
+plt.xlabel("Step")
+
 plt.show()
