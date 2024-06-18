c4854ae52246a7d15f594b4b5adbbe9739bded67
Joel Singh <joelsingh788@gmail.com>
2024-06-18 15:47:22 -0400

    Implement getAveragedAveragedRewards

--- main.py
+++ main.py
@@ -5,8 +5,21 @@ NUMBER_OF_RUNS: Final = 2000
 allAverageRewards: list[list[float]] = []
 allPercentageOfOptimalLeverChosen: list[list[float]] = []
 
+
+def getAveragedAveragedRewards():
+    averagedAveragedRewards: list[float] = []
+    for i in range(len(allAverageRewards[0])):
+        average = 0
+        for _,averageRewards in enumerate(allAverageRewards):
+            average += averageRewards[i]
+        average /= len(allAverageRewards)
+        averagedAveragedRewards.append(average)
+    return averagedAveragedRewards
+
 for i in range(NUMBER_OF_RUNS):
     singleRun = run(True)
     allAverageRewards.append(singleRun['averageRewards'])
     allPercentageOfOptimalLeverChosen.append(singleRun['percentageOfOptimalLeverChosen'])
     print(str(round(((i + 1) / NUMBER_OF_RUNS) * 100, 2)) + "%")
+
+print(getAveragedAveragedRewards())
